688c54db8f69ccd96aaba5aca950f57158c1098c
TomaszPielecki <tomasz.pielecki86@gmail.com>
2021-05-19 11:34:20 +0200

    dodanie pola person2

--- main.py
+++ main.py
@@ -1,22 +1,26 @@
 Person = {
-    'firstName':'Tomasz',
-    'secondName':'Pielecki',
-    'age':34,
-    'country':'poland',
-    'town':'Kartuzy',
-    'Job':'Car driver',
-    'status':'student'
+    'firstName': 'Tomasz',
+    'secondName': 'Pielecki',
+    'age': 34,
+    'country': 'poland',
+    'town': 'Kartuzy',
+    'Job': 'Car driver',
+    'status': 'student'
 }
 Person2 = {
-    'firstName':'Janusz',
-    'secondName':'Wierzbicki',
-    'age':54,
-    'country':'poland',
-    'town':'Malbork',
-    'Job':'Bus diriver',
-    'status':'worker'
+    'firstName': 'Janusz',
+    'secondName': 'Wierzbicki',
+    'age': 54,
+    'country': 'poland',
+    'town': 'Malbork',
+    'Job': 'Bus diriver',
+    'status': 'worker'
 }
 print(Person)
 print(Person2)
-print(Person['firstName'],':',Person['town'])
-print(Person2['firstName'],':',Person2['town'])
+print(Person['firstName'], ':', Person['town'])
+print(Person2['firstName'], ':', Person2['town'])
+# dodanie pola do person2
+Person2['car'] = 'Vw polo'
+
+print(Person2['firstName'], ':', Person2['town'], ':', Person2['car'])
