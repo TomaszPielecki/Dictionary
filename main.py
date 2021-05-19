775fac283b76d4846013cf393734f6bc94db1318
TomaszPielecki <tomasz.pielecki86@gmail.com>
2021-05-19 11:26:23 +0200

    Cw-Dictionary

---
+++ main.py
@@ -0,0 +1,22 @@
+Person = {
+    'firstName':'Tomasz',
+    'secondName':'Pielecki',
+    'age':34,
+    'country':'poland',
+    'town':'Kartuzy',
+    'Job':'Car driver',
+    'status':'student'
+}
+Person2 = {
+    'firstName':'Janusz',
+    'secondName':'Wierzbicki',
+    'age':54,
+    'country':'poland',
+    'town':'Malbork',
+    'Job':'Bus diriver',
+    'status':'worker'
+}
+print(Person)
+print(Person2)
+print(Person['firstName'],':',Person['town'])
+print(Person2['firstName'],':',Person2['town'])
